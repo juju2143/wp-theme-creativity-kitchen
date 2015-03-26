beb40db180ae86b8a525874abfdc46619a4b272d
Julien Savard <juju2143@gmail.com>
2015-03-26 00:33:32 +0000

    Mostly done, only some JS for the nav for the mobile site left to do and I think we're going in production.

--- header.php
+++ header.php
@@ -3,8 +3,9 @@
 <head>
 	<meta charset="utf-8" />
 	<title><?php wp_title('&laquo;', true, 'right'); bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-	<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
+	<link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/images/favicon.png" />
 	<?php wp_head(); ?>
 </head>
 <body>
@@ -12,6 +13,7 @@
 		<?php wp_nav_menu(array("theme_location"=>"menu_head","container"=>false)); ?>
 	</nav>
 	<header>
-		<img id="logo" src="<?php bloginfo('template_url'); ?>/images/logo.png" />
+		<div id="sandwich"><i class="fa fa-bars"></i></div>
+		<a href="<?php bloginfo('url'); ?>"><img id="logo" src="<?php bloginfo('template_url'); ?>/images/logo.png" /></a>
 		<div id="synopsis"><?php bloginfo('description'); ?></div>
 	</header>
